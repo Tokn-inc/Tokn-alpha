52dbadb5706c79229a0f3c3b559a611e6de7e7ea
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2021-12-10 21:30:38 -0800

    Initial Truffle Project setup

---
+++ MVP/contracts/mvpManager.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier:  MIT
+pragma solidity ^0.8.0;
+
+contract MVPManager{
+
+  mapping(uint256 => uint256) public _totalMinted;
+
+  constructor (){
+      }
+
+      function totalMinted(uint256 id) public view virtual returns (uint256) {
+          return _totalMinted[id];
+      }
+}
